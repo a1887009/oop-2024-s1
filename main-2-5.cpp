34c919b30440f3c9acd2335934e9405c18f8dd91
Kurt Brice <a1887009@student.adelaide.edu.au>
2024-02-28 19:39:06 +1030

    Medium questions upload2

--- main-2-5.cpp
+++ main-2-5.cpp
@@ -2,11 +2,11 @@
 
 // The `extern` keyword tells the compiler that the count function exists,
 // but will be implemented somewhere else
-extern bool is_decending(int array[], int n);
+extern bool is_descending(int array[], int n);
 
 int main() {
     int array[5] = {4,5,6,7,8};
     std::cout << std::boolalpha;
-    std::cout << "The array is in decending order: " << is_decending(array, 5) << std::endl;
+    std::cout << "The array is in decending order: " << is_descending(array, 5) << std::endl;
     return 0;
 }
