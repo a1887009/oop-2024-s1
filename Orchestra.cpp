cea8b17ed42ca39dc87e5daa79d097a6182d6934
Kurt Brice <a1887009@student.adelaide.edu.au>
2024-03-25 14:51:29 +1030

    commit4'
    clear

--- Orchestra.cpp
+++ Orchestra.cpp
@@ -2,22 +2,41 @@
 #include <string>
 #include "Orchestra.h"
 
-Orchestra::Orchestra() : Orchestra(0) {};
-Orchestra::Orchestra(int O_SIZE) {
+
+Orchestra::Orchestra() {members = new Musician[0]; curr_size = 0;}; // When no number defined it sets up for zero members
+
+Orchestra::Orchestra(int O_SIZE) { // when size defined it sets up for size
+    size = O_SIZE;
     members = new Musician[O_SIZE];
     curr_size = 0;
 };
 
+int Orchestra::get_current_number_of_members() {return curr_size;}; // returns the current size of members
+
 bool Orchestra::has_instrument(std::string O_INSTRUMENT) {
     for (int i = 0; i < size; i ++){
-        if (Musician::instrument == O_INSTRUMENT){
+        if (members[i].get_instrument() == O_INSTRUMENT){
             return true;
         }
     }
     return false;
 };
 
-members[curr_size++] = musician;
+bool Orchestra::add_musician(Musician new_musician) {
+    if (curr_size < size){
+        members[curr_size] = new_musician;
+        curr_size++;
+        return true;
+    }
+
+    return false;
+}
+
+Musician *Orchestra::get_members() {return members;};
+
+ Orchestra::~Orchestra() {delete[] members;};
+
+
 
 
 
