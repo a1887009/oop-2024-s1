9b6f81bd7ec797731df231b6d286e17bbaff349b
Kurt Brice <kurtbrice@Kurts-MacBook-Pro.local>
2024-05-20 11:16:01 +0930

    3

--- ws11.cpp
+++ ws11.cpp
@@ -17,43 +17,65 @@ private:
 public:
     RecordsManager(string filename) : _filename(filename) {}
     void read(Records &records) {
-        _file.open(_filename, ios::in);
-        if (_file.is_open()) {
+    
+            // _file.open(_filename, ios::in);
+        try{
+            _file.open(_filename, ios::in);
+            if (!_file.is_open()){
+                throw runtime_error(" Error in file");
+            }
+            if (_file.is_open()) {
             string line;
             while (std::getline(_file, line)) {
                 // try stoi if fails then catch and throw
                 try {
-                    int num = stoi(line);
+                    records.push_back(stoi(line));
                 }catch (const std::invalid_argument& e){
-                    std::cerr << "Invalid argument" << std::endl;
+                    throw runtime_error("Invalid argument");
+                    _file.close();
+                    // std::cerr << "Invalid argument" << std::endl;
                     break;
                 }catch (const std::out_of_range& e){
-                    std::cerr << "Out of range" << std::endl;
+                    // std::cerr << "Out of range" << std::endl;
+                    throw runtime_error("Out of range");
+                    _file.close();
                     break;
                 }
 
-                records.push_back(stoi(line));
             }
             _file.close();
         }
+        } catch (...){
+            _file.close();
+            throw;
+        }
+
+        //_file.open(_filename, ios::in);
+        
     }
 };
 
 int main() {
         
-        //RecordsManager receordM("test_clean.txt");
+        // RecordsManager receordM("test_clean.txt");
+        // RecordsManager receordM("test_corrupt1.txt");
         RecordsManager receordM("test_corrupt1.txt");
-        // RecordsManager receordM("test_corrupt2.txt");
         Records myRecords;
 
         // reads records
-        receordM.read(myRecords);
-
-        // calculate and print out the sum
-        int sum = 0;
-        for (int i = 0; i < myRecords.size(); i++) {
-             sum += myRecords[i];
+        try {
+            receordM.read(myRecords);
+       
+            // calculate and print out the sum
+            int sum = 0;
+            for (int i = 0; i < myRecords.size(); i++) {
+                sum += myRecords[i];
+            }
+            cout << sum << endl;
         }
-        cout << sum << endl;
+        catch(const exception &e)
+        {
+            std::cerr << "Error occurred:" << e.what() << std::endl;
+        }
     return 0;
 }
