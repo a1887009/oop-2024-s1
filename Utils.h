df7834bf537416e525a162e026180995c9f3c3a9
Kurt Brice <kurtbrice@Kurts-MacBook-Pro.local>
2024-05-24 14:42:05 +0930

    3

--- Utils.h
+++ Utils.h
@@ -8,14 +8,14 @@
 
 class Utils{
 public:
-    std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight) {
+    static std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight) {
         std::tuple<int, int> position;
         std::get<0>(position) = rand() % (gridWidth + 1);
         std::get<1>(position) = rand() % (gridHeight + 1);
         return position;
     };
 
-        double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2) {
+        static double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2) {
         double distance;
         distance = pow(pow(std::get<0>(pos1) - std::get<0>(pos2), 2) + pow(std::get<1>(pos1) - std::get<1>(pos2), 2), 0.5);
         return distance;
--- main-1.cpp
+++ main-1.cpp
@@ -9,19 +9,16 @@ int main() {
     int gridWidth = 10;
     int gridHeight = 10;
 
-    // Create Utils object
-    Utils utils;
-
     // Generate random positions
-    std::tuple<int, int> pos1 = utils.generateRandomPos(gridWidth, gridHeight);
-    std::tuple<int, int> pos2 = utils.generateRandomPos(gridWidth, gridHeight);
+    std::tuple<int, int> pos1 = Utils::generateRandomPos(gridWidth, gridHeight);
+    std::tuple<int, int> pos2 = Utils::generateRandomPos(gridWidth, gridHeight);
 
     // Create GameEntities with random positions
     GameEntity entity1(std::get<0>(pos1), std::get<1>(pos1), 'A');
     GameEntity entity2(std::get<0>(pos2), std::get<1>(pos2), 'B');
 
     // Calculate distance between entities
-    double distance = utils.calculateDistance(pos1, pos2);
+    double distance = Utils::calculateDistance(pos1, pos2);
 
     // Display information
     std::cout << "Entity 1: Type " << entity1.getType() << ", Position: (" << std::get<0>(pos1) << ", " << std::get<1>(pos1) << ")\n";
