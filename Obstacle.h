6c54aa78428f98952299b9b473d0ad27684429a9
Kurt Brice <kurtbrice@Kurts-MacBook-Pro.local>
2024-05-30 16:49:06 +0930

    q2 4

--- Obstacle.h
+++ Obstacle.h
@@ -5,13 +5,15 @@
 #include "Modifier.h"
 
 class Obstacle: public GridUnit, public Modifier{
+private: 
+    bool active;
 public:
-    Obstacle(int x, int y) : GridUnit(x, y, 'O') {};
+    Obstacle(int x, int y) : active(true), GridUnit(x, y, 'O') {};
     bool isActive(){
-        if (getEntity() == 'O'){return true;}
+        if (active){return true;}
         return false;
     }
-    void apply(GridUnit& unit) {unit.setEntity('X'); setEntity('X');}
+    void apply(GridUnit& unit) {unit.setEntity('O'); active = false;}
 };
 
 
